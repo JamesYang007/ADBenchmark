077acb7541fa06439cbfd26eef341b420525997a
James Yang <jamesyang916@gmail.com>
2020-08-26 20:20:54 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AD Benchmark
+
+This is a repository dedicated to benchmarking automatic differentiation libraries for research purposes.
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+fastadpath="lib/FastAD"
+
+# setup FastAD
+if [ ! -d "$fastadpath" ]; then
+    git clone https://github.com/JamesYang007/FastAD.git $fastadpath
+    cd $fastadpath
+    ./setup.sh
+    ./clean-build.sh release -DFASTAD_ENABLE_TEST=OFF \
+        -DCMAKE_INSTALL_PREFIX=".." # installs into build
+    cd build/release
+    ninja install
+    cd ../../ # in lib/FastAD
+    cd ../../ # in working directory
+fi
